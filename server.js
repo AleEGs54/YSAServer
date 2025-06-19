2d8357f43261efa49afb3def7d7a239639b3c889
Alejandro Esteves <alejandro.esteves19@gmail.com>
2025-06-18 17:12:20 -0700

    Add ms, object-assign, on-headers, random-bytes, and uid-safe packages
    - Added ms package for time format conversion.
    - Introduced object-assign for ES2015 Object.assign() ponyfill.
    - Included on-headers to execute a listener when a response is about to write headers.
    - Integrated random-bytes for generating strong pseudo-random bytes.
    - Added uid-safe for creating cryptographically secure UIDs safe for cookie and URL usage.

--- server.js
+++ server.js
@@ -2,10 +2,40 @@ const express = require("express");
 require("dotenv").config()
 const app = express();
 const participantsRoute = require("./routes/participantRoute")
+const session = require('express-session')
+const pool = require("./database")
+const cors = require('cors')
 
 /* ******************************************
- * Middlewares
- * ***************************************** */
+* Middlewares
+* ***************************************** */
+
+//Builds a session into the database so the user doesn't have to login every request.
+app.use(session({
+    store: new (require('connect-pg-simple')(session))({
+        createTableIfMissing: true,
+        pool,
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    name: 'sessionId',
+}))
+
+/* ******************************************
+* CORS Configuration
+* ***************************************** */
+const whitelist = ['http://localhost:3000']
+
+app.use(cors((origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+        callback(null, true)
+    } else {
+        callback(new Error('Not allowed by CORS'))
+    }
+}))
+
+app.options('*', cors())
 
 //To receive JSON
 app.use(express.json())
